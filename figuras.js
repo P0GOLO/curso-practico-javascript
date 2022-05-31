b9b364297c89fba00c36ceb53b4035a7de0f02f9
pogolo <pogolo@LAPTOP-BEUR4A80.localdomain>
2022-05-31 09:46:06 -0500

    Solo falta el círculo

---
+++ figuras.js
@@ -0,0 +1,44 @@
+// Código del cuadaro
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+// Siempre haz tu código autodescriptivo
+const perimetroCuadrado = ladoCuadrado * 4
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+console.log("El área del cuadrado es: " + areaCuadrado + "cm^2"); 
+console.groupEnd("");
+
+// Código del triángulo 
+console.group("Triángulos");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo3 = 4;
+
+console.log(
+    "Los lados del triángulo miden: " 
+    + ladoTriangulo1
+    + "cm, " 
+    + ladoTriangulo2 
+    + "cm, " 
+    + baseTriangulo3 
+    + "cm"
+); 
+const alturaTriangulo =5.5;
+console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo3
+console.log("El perímetro del triángulo es: "+ perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo3 * alturaTriangulo) / 2;
+console.log("El área del Triángulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd("");
+
+// Código del círculo
+console.group("Circulos");
+const 
+
+console.groupEnd("");
