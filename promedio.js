3baa93d57e43d41a02fcccc6a507ff74f3044fc8
P0GOLO <paquitodedos@gmail.com>
2022-07-21 22:46:52 -0500

    Reduce Sustituto de for (Ya hay funciones)

--- promedio.js
+++ promedio.js
@@ -1,11 +1,18 @@
-const lista1 = [
-    100, 200, 300, 500
-];
 
-let sumaLista1 = 0;
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    // }
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    
+    const promedioLista = sumaLista / lista.length;
 
-for (let i = 0; i < lista1.length; i++) {
-    sumaLista1 = sumaLista1 + lista1[i];
+    return promedioLista
 }
 
-const promedioLista1 = sumaLista1 / lista1.length;
