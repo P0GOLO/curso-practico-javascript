d4de75f643a50d2718d0ba94fa58d594dca3a8cb
P0GOLO <paquitodedos@gmail.com>
2022-07-21 22:20:04 -0500

    Promedio, Media(creo) Listos en Consola |Final|

--- promedio.js
+++ promedio.js
@@ -1,9 +1,11 @@
-const sumaLista1 = [
+const lista1 = [
     100, 200, 300, 500
 ];
 
-let sumaLista = 0;
+let sumaLista1 = 0;
 
 for (let i = 0; i < lista1.length; i++) {
     sumaLista1 = sumaLista1 + lista1[i];
 }
+
+const promedioLista1 = sumaLista1 / lista1.length;
